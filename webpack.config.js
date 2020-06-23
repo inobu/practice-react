076fff82c80b5312a79e8fd4f74e6b0c50b309c5
inobu <inohkunobuya@gmail.com>
2020-06-23 22:36:22 +0900

    iniial commmit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: "development",
+  entry: './src/index.tsx',
+  output: {
+    filename: './dist/bundle.js'
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: [
+          {
+            loader: 'ts-loader'
+          },
+
+          
+        ]
+      }
+    ],
+  }
+};
